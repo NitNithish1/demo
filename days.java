c3d582a6bf0160fc11cc2da87de9815b91790ef2
NitNithish1 <knitheeswaran3@gmail.com>
2025-09-03 17:33:01 +0530

    Create days.java

---
+++ days.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class witch {
+    public static void main(String[] args) {
+    Scanner bk =new Scanner(System.in);
+    int i=bk.nextInt();
+    int choice;
+    switch(i){
+        case 1:System.out.println("monday");break;
+        case 2:System.out.println("tuesday");break;
+        case 3:System.out.println("wednesday");break;
+        case 4:System.out.println("thursday");break;
+        case 5:System.out.println("friday");break;
+        case 6:System.out.println("sunday");break;
+        default:System.out.println("vinayagar chadhurthi");
+    }
+
+
+
+    }
+    
