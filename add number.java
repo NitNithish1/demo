570d31e992104b2d4fe3ff3a6393c61bf0196302
NitNithish1 <knitheeswaran3@gmail.com>
2025-09-11 18:28:30 +0530

    Create add number.java

---
+++ add number.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class odd {
+        public static void main(String[] args) {
+        Scanner bk =new Scanner(System.in);
+        int n=bk.nextInt();
+        if(n%2==0)
+        System.out.println("even");
+    else
+        System.out.println("odd");
+        }
+
+    
+}
