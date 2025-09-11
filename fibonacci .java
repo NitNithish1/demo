59024f18296f4e4049f6b9b5cb185059bc57a392
NitNithish1 <knitheeswaran3@gmail.com>
2025-09-11 18:31:20 +0530

    Create  fibonacci .java

---
+++ fibonacci .java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class fibonacci {
+    public static void main(String[] args) {
+        Scanner bk =new Scanner(System.in);
+        System.out.println("enter the number of column");
+        int n=bk.nextInt();
+        for (int i=1; i<=n; i++) {
+            for(int j=1;j<=n;j++)
+            System.out.println(i);
+                  }
+             System.out.println();   
+            
+        }
+
+    }
+    
