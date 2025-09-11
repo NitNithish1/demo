5df052277135df5d42a9efc6e2fe6939a3ea6649
NitNithish1 <knitheeswaran3@gmail.com>
2025-09-11 18:32:49 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+public class even {
+    public static void main(String[] args) {
+        Scanner ns =new Scanner(System.in);
+        int i = ns.nextInt();
+        System.out.println("");
+        
+    }
+
+    }
+    
