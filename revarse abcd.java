87411bdd6d03837f12fc862452f8382945b6abfc
K NITHEESWARAN <knitheeswaran3@gmail.com>
2025-09-14 22:23:24 +0530

    Create revarse abcd.java

---
+++ revarse abcd.java
@@ -0,0 +1,9 @@
+public class abcd
+{
+	public static void main(String[] args) {
+	    // Scanner scan = new Scanner(System.in);
+        // char n = scan.next().charAt(0);
+        for(char i='Z';i>='A';i--)
+        System.out.print(i+" ");
+    }
+}
