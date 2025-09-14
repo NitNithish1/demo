bf656874a129e397904843c09a1c0ea737c17132
K NITHEESWARAN <knitheeswaran3@gmail.com>
2025-09-14 22:16:05 +0530

    Create abcd.java

---
+++ abcd.java
@@ -0,0 +1,9 @@
+public class abcd
+{
+	public static void main(String[] args) {
+	    // Scanner scan = new Scanner(System.in);
+        // char n = scan.next().charAt(0);
+        for(char i='A';i<='Z';i++)
+        System.out.print(i+" ");
+    }
+}
